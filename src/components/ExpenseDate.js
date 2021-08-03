c957b73ac08daa05ad2f04e354a0c0610bf64526
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-03 13:05:35 +0200

    Task 8: splitting components into multiple components

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './ExpenseDate.css';
+
+export const ExpenseDate = (props) => {
+
+    const day=props.date.toLocaleString('en-US',{day:'numeric'});
+    const month=props.date.toLocaleString('en-US',{month:'long'});
+    const year=props.date.toLocaleString('en-US',{year:'numeric'});
+
+    return (
+        <div>
+            <div className="expense-date">
+                <div className="expense-date__month">{month}</div>
+                <div className="expense-date__year">{year}</div>
+                <div className="expense-date__day">{day}</div>
+            </div>
+        </div>
+    )
+}
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,12 @@
 import React from 'react'
+import { ExpenseDate } from './ExpenseDate';
 import './ExpenseItem.css';
 
 export const ExpenseItem = (props) => {
     
-    const day=props.date.toLocaleString('en-US',{day:'numeric'});
-    const month=props.date.toLocaleString('en-US',{month:'long'});
-    const year=props.date.toLocaleString('en-US',{year:'numeric'});
-    
     return (
         <div className="expense-item">
-            <div className="expense-date">
-                <div className="expense-date__month">{month}</div>
-                <div className="expense-date__year">{year}</div>
-                <div className="expense-date__day">{day}</div>
-            </div>
+            <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
