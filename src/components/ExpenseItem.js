cf57c0d71a88410a19643e8d417a7b8179b8b2c7
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-03 09:54:19 +0200

    Task 2: add expense-item component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ExpenseItem = () => {
+    return (
+        <div>
+            <h2>Expense Item !!!</h2>
+        </div>
+    )
+}
+
+export default ExpenseItem;
