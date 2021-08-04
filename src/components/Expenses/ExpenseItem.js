b8bc3e1821c823cb0a732aa48984ffe4d33b6086
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-04 11:14:30 +0200

    Task 11: working with state (useState())

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React ,{useState} from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
-
+import faker from 'faker';
 export const ExpenseItem = (props) => {
-    
+
+    //let title=props.title;
+    const [title, setTitle] = useState(props.title);
+
+
+    const clickHandler=()=>{
+        //title='Hello from react';
+        setTitle(faker.commerce.productName());
+        console.log("From clickHandler- title is: ",title);
+    }
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     )
 }
