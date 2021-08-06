8be1a97bd5baf8e816ced7755b656866d8b04c68
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-06 17:46:16 +0200

    Task 35: styled components & dynamic props trick 2

--- 2-react-course-goals-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 2-react-course-goals-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -10,12 +10,14 @@ margin: 0.5rem 0;
   font-weight: bold;
   display: block;
   margin-bottom: 0.5rem;
+  color: ${props => props.invalid ? 'red' : 'black'};
 }
 
 & input {
   display: block;
   width: 100%;
-  border: 1px solid #ccc;
+  border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+  background:${props => props.invalid ? '#ffd7d7' : 'white'};
   font: inherit;
   line-height: 1.5rem;
   padding: 0 0.25rem;
@@ -25,15 +27,6 @@ margin: 0.5rem 0;
   outline: none;
   background: #fad0ec;
   border-color: #8b005d;
-}
-
-&.invalid input{
-  border-color: red;
-  background: #ffd7d7;
-}
-
-&.invalid label{
-  color: red;
 }
 `;
 const CourseInput = props => {
@@ -58,9 +51,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={!isValid && 'invalid'}>
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
-        <input type="text" />
+        <input type="text" onChange={goalInputChangeHandler}/>
       </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
