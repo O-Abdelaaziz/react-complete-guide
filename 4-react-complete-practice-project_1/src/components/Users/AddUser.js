4e191feba2577f8beca7ae317933f41ca355eb2e
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-07 18:21:47 +0200

    Task 49: managing the error state

--- 4-react-complete-practice-project_1/src/components/Users/AddUser.js
+++ 4-react-complete-practice-project_1/src/components/Users/AddUser.js
@@ -8,6 +8,7 @@ const AddUser = (props) => {
 
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
 
     const usernameChangeHandler=(event)=>{
         setEnteredUsername(event.target.value);
@@ -22,9 +23,17 @@ const AddUser = (props) => {
     const submitHandler=(event)=>{
         event.preventDefault();
         if(enteredUsername.trim().length===0 || enteredAge.trim().length ===0){
+            setError({
+                title:'Invalid input',
+                message:'Please enter a valid name and age (non-empty values).'
+            })
             return;
         }
         if(+enteredAge<1){
+            setError({
+                title:'Invalid input',
+                message:'Please enter a valid age (non-negative value).'
+            })
             return;
         }
         props.onAddUser(enteredUsername,enteredAge);
@@ -32,9 +41,14 @@ const AddUser = (props) => {
         setEnteredAge('');
     }
 
+    const errorHandler=()=>{
+        setError(null);
+    }
+
     return (
         <div>
-            <ErrorModal title="An error occured!" message="Something went wrong!" />
+            {error && <ErrorModal title={error.title} message={error.message}   onConfirm={errorHandler}/>}
+            
             <Card className={classes.input}>
                 <form onSubmit={submitHandler}>
                     <label htmlFor="username">Username:</label>
