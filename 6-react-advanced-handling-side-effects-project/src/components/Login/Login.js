ee6493edbc3d3fdd35ded2362192d0384d4ae6d1
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-09 12:00:46 +0200

    Task 60: using the use-effect cleanup function

--- 6-react-advanced-handling-side-effects-project/src/components/Login/Login.js
+++ 6-react-advanced-handling-side-effects-project/src/components/Login/Login.js
@@ -12,12 +12,19 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+
+    const identifier = setTimeout(() => {
+          console.log('Checking the form validity');
+          setFormIsValid(
+            enteredEmail.includes('@') && enteredPassword.trim().length > 6
+          );
+        }, 500);
+    return ()=>{
+      console.log('Clean up the timer');
+      clearTimeout(identifier);
+    }    
 
   }, [enteredEmail,enteredPassword])
-
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
