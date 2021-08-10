335bee881989deb6770988c92ab2d095d5524d65
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-10 21:54:27 +0200

    Task 65: making context dynamic when passing value

--- 6-react-advanced-handling-side-effects-project/src/components/MainHeader/Navigation.js
+++ 6-react-advanced-handling-side-effects-project/src/components/MainHeader/Navigation.js
@@ -3,26 +3,26 @@ import AuthContext from '../../store/auth-context';
 
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
+const Navigation = () => {
   
   const context = useContext(AuthContext)
 
   return (
     <nav className={classes.nav}>
       <ul>
-        {context && (
+        {context.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {context && (
+        {context.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {context && (
+        {context.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={context.onLogout}>Logout</button>
           </li>
         )}
       </ul>
