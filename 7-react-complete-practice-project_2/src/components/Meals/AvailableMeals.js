ce72ae7010326d569b1b00d9dbec4457bda7daa7
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-12 09:00:46 +0200

    Task 73: adding individual (meal-items, card) & displaying them

--- 7-react-complete-practice-project_2/src/components/Meals/AvailableMeals.js
+++ 7-react-complete-practice-project_2/src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
+import MealsItem from './MealsItem/MealsItem';
 
 const DUMMY_MEALS = [
     {
@@ -29,10 +31,16 @@ const DUMMY_MEALS = [
   ];
 
 const AvailableMeals = () => {
-    const meals =DUMMY_MEALS.map(meal =>(<li>{meal.name}</li>))
+    const meals =DUMMY_MEALS.map(meal =>(<MealsItem 
+      key={meal.id} 
+      name={meal.name} 
+      description={meal.description}
+      price={meal.price} />))
     return (
         <section className={classes.meals}>
-            {meals}
+            <Card>
+             <ul>{meals}</ul>
+            </Card>
         </section>
     )
 }
