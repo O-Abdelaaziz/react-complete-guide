77c13d7cf524ea77ce6117e32d4e5b32d2fef2f9
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-14 17:13:56 +0200

    Task 92: use use-callback and its dependencies (in array)

--- 8-react-a-look-behind-thescenes-optimization-techniques/src/App.js
+++ 8-react-a-look-behind-thescenes-optimization-techniques/src/App.js
@@ -7,16 +7,24 @@ import Button from './components/UI/Button/Button';
 function App() {
   console.log('App is running');
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   const toggleParagraphHandler = useCallback(()=>{
-     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  },[]);
+    if (allowToggle){
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  },[allowToggle]);
+
+  const AllowToggleHandler = (()=>{
+    setAllowToggle(true)
+  })
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutPut show={false}/>
-      <Button onClick={toggleParagraphHandler} >Toggle Paragraph !!!</Button>
+      <DemoOutPut show={showParagraph}/>
+      <Button onClick={AllowToggleHandler} >Allow Toggle !!!</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph !!!</Button>
     </div>
   );
 }
