c0aa0f11e85240fcf18224360b2de28efe57466e
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-14 10:17:16 +0200

    Task 88: component updates in action (how element render in dom)

--- 8-react-a-look-behind-thescenes-optimization-techniques/src/App.js
+++ 8-react-a-look-behind-thescenes-optimization-techniques/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
 
 function App() {
+
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = ()=>{
+     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  }
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <Button onClick={toggleParagraphHandler} >Toggle Paragraph !!!</Button>
+      {showParagraph && <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>}
     </div>
   );
 }
