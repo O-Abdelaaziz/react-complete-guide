3ef690ff72e0b46e9a09aa60407b93bbba7fbd85
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-15 14:06:26 +0200

    Task 97: class-based components & context

--- 9-react-alternative-way-of-building-components/src/components/UserFinder.js
+++ 9-react-alternative-way-of-building-components/src/components/UserFinder.js
@@ -1,14 +1,11 @@
 import { Fragment, useState, useEffect, Component } from 'react';
+import UsersContext from '../store/users-context';
 import classes from './UserFinder.module.css'
 import Users from './Users';
-
-const DUMMY_USERS = [
-    { id: 'u1', name: 'Max' },
-    { id: 'u2', name: 'Manuel' },
-    { id: 'u3', name: 'Julie' },
-  ];
-
 class UserFinder extends Component{
+
+    static contextType=UsersContext;
+
     constructor(){
         super();
         this.state={
@@ -24,14 +21,14 @@ class UserFinder extends Component{
 
     componentDidMount(){
         this.setState({
-            filteredUsers:DUMMY_USERS
+            filteredUsers:this.context.users
         });
     }
 
     componentDidUpdate(prevProps,prevState){
         if(prevState.searchTerm !== this.state.searchTerm){
             this.setState({
-                filteredUsers:DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm)),
+                filteredUsers:this.context.users.filter((user) => user.name.includes(this.state.searchTerm)),
             });
         }
     }
