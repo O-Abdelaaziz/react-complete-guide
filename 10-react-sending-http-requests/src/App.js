613b9632caf62c93fa2277913996ee1b7d200f3e
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-15 22:56:14 +0200

    Task 105: sending a POST request

--- 10-react-sending-http-requests/src/App.js
+++ 10-react-sending-http-requests/src/App.js
@@ -1,76 +1,82 @@
-import React, {useEffect, useState,useCallback} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
-
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchMoviesHandler= useCallback(async () => {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response =await fetch('https://swapi.dev/api/films');
-      
-      if(!response.ok){
+      const response = await fetch('https://react-http-6b4a6.firebaseio.com/movies.json');
+      if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
-      const data= await response.json();
+      const data = await response.json();
 
-      const transformedMovies=data.results.map(movie=>{
-        return{
-          id:movie.episode_id,
-          title:movie.title,
-          releaseDate:movie.release_date,
-          openingText:movie.opening_crawl,
+      const loadedMovies = [];
 
-        };
-      });
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
 
-    setMovies(transformedMovies);
-
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
+  }, []);
 
-   },[]);
-
-   useEffect(() => {
+  useEffect(() => {
     fetchMoviesHandler();
-
   }, [fetchMoviesHandler]);
 
-  // you can use this and avoid inline check section element
-  //  let content = <p>Found no movies.</p>;
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://react-http-6b4a6.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+  }
 
-  //  if (movies.length > 0) {
-  //    content = <MoviesList movies={movies} />;
-  //  }
- 
-  //  if (error) {
-  //    content = <p>{error}</p>;
-  //  }
- 
-  //  if (isLoading) {
-  //    content = <p>Loading...</p>;
-  //  }
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        {!isLoading && movies.length>0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length===0 && !error && <p>found no movies!</p>}
-        {!isLoading && error && <p>{error}</p>}
-        {isLoading && <p>is loading ...</p>}
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
