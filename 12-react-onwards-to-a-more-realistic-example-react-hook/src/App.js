88e0679707d2836aa02541b1aacb199bd539e189
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-16 13:49:30 +0200

    Task 114: adjusting the custom hook logic

--- 12-react-onwards-to-a-more-realistic-example-react-hook/src/App.js
+++ 12-react-onwards-to-a-more-realistic-example-react-hook/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState ,useCallback} from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
@@ -8,23 +8,23 @@ function App() {
   
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks= taskObj=>{
-    const loadedTasks = [];
-
-    for (const taskKey in taskObj) {
-      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  }
-
-  const httpData = useHttp({url:'https://react-http-c7642-default-rtdb.firebaseio.com/tasks.json'},transformTasks);
+  const httpData = useHttp();
 
   const {isLoading, error, sendRequest:fetchTasks} = httpData;
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks=(taskObj=>{
+      const loadedTasks = [];
+  
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+  
+      setTasks(loadedTasks);
+    });
+
+    fetchTasks({url:'https://react-http-c7642-default-rtdb.firebaseio.com/tasks.json'},transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
