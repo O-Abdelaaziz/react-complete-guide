69c56f45325ab23b0168fdcf4d7c7378c06e2273
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-17 12:15:32 +0200

    Task 125: a practice & challenge time with basic-form component (use-state hook)

--- 13-react-working-with-forms-and-user-input/src/components/BasicForm.js
+++ 13-react-working-with-forms-and-user-input/src/components/BasicForm.js
@@ -1,22 +1,91 @@
+import { useState } from "react";
+
 const BasicForm = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+
+  const isEnteredNameIsValid=enteredName.trim() !=='';
+  const nameInputIsInvalid= !isEnteredNameIsValid && enteredNameTouched;
+
+  const isEnteredEmailIsValid=enteredEmail.includes('@');
+  const emailInputIsInvalid= !isEnteredEmailIsValid && enteredEmailTouched;
+
+  let isFormValid=false;
+
+  if(isEnteredNameIsValid && isEnteredEmailIsValid){
+    isFormValid=true;
+  }
+
+  const nameInputChangeHandler=(event)=>{
+    setEnteredName(event.target.value);
+  }
+
+  const nameBlurHandler=(event)=>{
+    setEnteredNameTouched(true);
+  }
+
+  const emailInputChangeHandler=(event)=>{
+    setEnteredEmail(event.target.value);
+  }
+
+  const emailBlurHandler=(event)=>{
+    setEnteredEmailTouched(true);
+  }
+  
+
+  const submitFormHandler=(event)=>{
+    event.preventDefault();
+
+    setEnteredNameTouched(true);
+    setEnteredEmailTouched(true);
+
+    if(!isEnteredNameIsValid || !isEnteredEmailIsValid){
+      return;
+    }
+
+    setEnteredName('');
+    setEnteredNameTouched(false);
+
+    setEnteredEmail('');
+    setEnteredEmailTouched(false);
+  }
+
+  const nameInputClassInvalid= nameInputIsInvalid ? 'form-control invalid' : 'form-control';
+  const emailInputClassInvalid= emailInputIsInvalid ? 'form-control invalid' : 'form-control';
+  
   return (
-    <form>
+    <form onSubmit={submitFormHandler}>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={nameInputClassInvalid}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input 
+          id='name'
+          value={enteredName} 
+          type='text'
+          onChange={nameInputChangeHandler}
+          onBlur={nameBlurHandler}   />
+          {nameInputIsInvalid && (<p className="error-text">input name is not valid.</p>)}
         </div>
         <div className='form-control'>
           <label htmlFor='name'>Last Name</label>
           <input type='text' id='name' />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={emailInputClassInvalid}>
+        <label htmlFor='email'>E-Mail Address</label>
+        <input  
+          id='email'
+          value={enteredEmail} 
+          type='email'
+          onChange={emailInputChangeHandler}
+          onBlur={emailBlurHandler}   />
+          {emailInputIsInvalid && (<p className="error-text">input email is not valid.</p>)}
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!isFormValid}>Submit</button>
       </div>
     </form>
   );
