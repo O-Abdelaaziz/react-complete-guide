ac170beb870103911e9166f1ccc6d056c6533c46
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-17 17:39:01 +0200

    Task 129: fetching meals via http

--- 14-react-complete-practice-project_3/src/components/Meals/AvailableMeals.js
+++ 14-react-complete-practice-project_3/src/components/Meals/AvailableMeals.js
@@ -1,3 +1,4 @@
+import {useEffect, useState} from "react";
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
@@ -30,7 +31,31 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals=async ()=>{
+      const response =await fetch('https://react-http-c7642-default-rtdb.firebaseio.com/meals.json');
+      const responseData=await response.json();
+
+      const loadMeals=[];
+
+      for(const key in responseData){
+        loadMeals.push({
+          id:key,
+          name:responseData[key].name,
+          description:responseData[key].description,
+          price:responseData[key].price,
+        });
+      }
+      setMeals(loadMeals);
+    };
+
+    fetchMeals();
+
+  },[]);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
