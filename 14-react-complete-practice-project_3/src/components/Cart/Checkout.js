c49ba01dd9eb41f3eea90be116ac58346e8455c8
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-18 09:58:06 +0200

    Task 133: reading form values using use-ref hook

--- 14-react-complete-practice-project_3/src/components/Cart/Checkout.js
+++ 14-react-complete-practice-project_3/src/components/Cart/Checkout.js
@@ -1,29 +1,38 @@
-import React from 'react'
+import { useRef } from 'react';
 import classes from './Checkout.module.css';
 
 const Checkout = (props) => {
-    
-    const confirmHandler = (event) => {
-        event.preventDefault();
-    };
 
-    return (
-        <form className={classes.form} onSubmit={confirmHandler}>
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+    const enteredName=nameInputRef.current.value;
+    const enteredStreet=streetInputRef.current.value;
+    const enteredPostalcode=postalCodeInputRef.current.value;
+    const enteredCity=cityInputRef.current.value;
+  };
+
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameInputRef} type='text' id='name' />
       </div>
       <div className={classes.control}>
         <label htmlFor='street'>Street</label>
-        <input type='text' id='street' />
+        <input ref={streetInputRef} type='text' id='street' />
       </div>
       <div className={classes.control}>
         <label htmlFor='postal'>Postal Code</label>
-        <input type='text' id='postal' />
+        <input ref={postalCodeInputRef} type='text' id='postal' />
       </div>
       <div className={classes.control}>
         <label htmlFor='city'>City</label>
-        <input type='text' id='city' />
+        <input ref={cityInputRef} type='text' id='city' />
       </div>
       <div className={classes.actions}>
         <button type='button' onClick={props.onCancel}>
@@ -32,6 +41,7 @@ const Checkout = (props) => {
         <button className={classes.submit}>Confirm</button>
       </div>
     </form>
-    )
-}
-export default Checkout; 
+  );
+};
+
+export default Checkout;
