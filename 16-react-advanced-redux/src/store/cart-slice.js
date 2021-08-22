c8079110c3cff8480ec93bc3a7f3c6ac097cf0a2
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-22 22:07:05 +0200

    Task 159: getting started with fetching data

--- 16-react-advanced-redux/src/store/cart-slice.js
+++ 16-react-advanced-redux/src/store/cart-slice.js
@@ -10,6 +10,10 @@ const cartSlice = createSlice({
     name: 'cart',
     initialState: initialCartState,
     reducers: {
+        replaceCart(state, action){
+            state.totalQuantity=action.payload.totalQuantity;
+            state.items=action.payload.items;
+        },
         addItemToCart(state, action) {
             const newItem = action.payload;
             const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -41,43 +45,6 @@ const cartSlice = createSlice({
     }
 });
 
-export const sendCartData = (cart) => {
-    return async (dispatch) => {
-        dispatch(uiActions.showNotification({
-            status: 'Pending',
-            title: 'Sending...',
-            message: 'Sending cart data !'
-        }));
-
-        const sendRequest = async () => {
-            const response = await fetch('https://react-http-c7642-default-rtdb.firebaseio.com/cart.json', {
-                method: 'PUT',
-                body: JSON.stringify(cart),
-            });
-
-            if (!response.ok) {
-                throw new Error('Sending cart data failed.');
-            }
-        }
-
-
-        try {
-            await sendRequest();
-            dispatch(uiActions.showNotification({
-                status: 'Success',
-                title: 'Success...',
-                message: 'Sending cart data successfully.'
-            }));
-        } catch (error) {
-            dispatch(uiActions.showNotification({
-                status: 'Error',
-                title: 'Error...',
-                message: 'Sending cart data failed.'
-            }));
-        }
-
-    }
-}
 
 export const cartActions = cartSlice.actions;
 export default cartSlice.reducer;
