e78bd1f3897d1f02ca2367787ce59336ee11f035
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-22 17:57:34 +0200

    Task 155: using use-effect with redux

--- 16-react-advanced-redux/src/App.js
+++ 16-react-advanced-redux/src/App.js
@@ -1,14 +1,23 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
-  const stateCart = useSelector(state => state.ui.cartIsVisible);
+  const stateUi = useSelector(state => state.ui.cartIsVisible);
+  const stateCart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-http-c7642-default-rtdb.firebaseio.com/cart.json',{
+      method:'PUT',
+      body:JSON.stringify(stateCart),
+    })
+  }, [stateCart])
 
   return (
     <Layout>
-      {stateCart && <Cart />}
+      {stateUi && <Cart />}
       <Products />
     </Layout>
   );
