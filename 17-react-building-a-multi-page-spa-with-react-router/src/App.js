831980516d37e3045e1638e6caafc414260ba420
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-23 11:10:03 +0200

    Task 163: defining & using routes

--- 17-react-building-a-multi-page-spa-with-react-router/src/App.js
+++ 17-react-building-a-multi-page-spa-with-react-router/src/App.js
@@ -1,7 +1,13 @@
+import { Route } from "react-router-dom";
+import Product from "./components/Product";
+import Welcome from "./components/Welcome";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Route path='/welcome'><Welcome/></Route>
+      <Route path='/product'><Product/></Route>
     </div>
   );
 }
---
+++ 17-react-building-a-multi-page-spa-with-react-router/src/components/Product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = () => {
+    return (
+        <div>
+            <h1>Hello From Product Component.</h1>
+        </div>
+    )
+}
+
+export default Product
