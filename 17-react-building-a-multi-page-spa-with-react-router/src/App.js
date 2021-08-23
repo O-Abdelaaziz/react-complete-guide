c5f0bbd4c477765ad523d5efe1844b196d90d3f7
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-23 11:44:57 +0200

    Task 164: working with links (link component from react-router-dom)

--- 17-react-building-a-multi-page-spa-with-react-router/src/App.js
+++ 17-react-building-a-multi-page-spa-with-react-router/src/App.js
@@ -1,11 +1,12 @@
 import { Route } from "react-router-dom";
-import Product from "./components/Product";
-import Welcome from "./components/Welcome";
-
+import MainHeader from "./components/MainHeader";
+import Product from "./pages/Product";
+import Welcome from "./pages/Welcome";
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <MainHeader/>
       <Route path='/welcome'><Welcome/></Route>
       <Route path='/product'><Product/></Route>
     </div>
