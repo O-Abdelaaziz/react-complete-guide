b644c538398fe11d1ebe7631a0f5f385bf22471e
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-23 12:45:03 +0200

    Task 167: extracting route params using react-router-dom hook (useParams())

--- 17-react-building-a-multi-page-spa-with-react-router/src/pages/ProductDetails.js
+++ 17-react-building-a-multi-page-spa-with-react-router/src/pages/ProductDetails.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const ProductDetails = () => {
+    const param = useParams();
+    console.log(param.productId);
     return (
         <section>
            <h1>Product Details</h1>
+           <p>{param.productId}</p>
         </section>
     )
 }
