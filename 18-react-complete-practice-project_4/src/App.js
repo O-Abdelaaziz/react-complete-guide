b58a9429f2a06322ee72569a8cfb6dee82c8de5f
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-23 20:08:27 +0200

    Task 174: adding a layout wrapper component

--- 18-react-complete-practice-project_4/src/App.js
+++ 18-react-complete-practice-project_4/src/App.js
@@ -1,3 +1,4 @@
+import Layout from "./components/layout/Layout";
 import AllQuotes from "./components/pages/AllQuotes";
 import NewQuote from "./components/pages/NewQuote";
 import QuoteDetail from "./components/pages/QuoteDetail";
@@ -5,13 +6,14 @@ const { Route, Switch, Redirect } = require("react-router-dom");
 
 function App() {
   return (
-    <Switch>
-      
-      <Route path='/' exact><Redirect to='/quotes'/></Route>
-      <Route path='/quotes' exact><AllQuotes/></Route>
-      <Route path='/quotes/:quoteId'><QuoteDetail/></Route>
-      <Route path='/new-quote'><NewQuote/></Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path='/' exact><Redirect to='/quotes' /></Route>
+        <Route path='/quotes' exact><AllQuotes /></Route>
+        <Route path='/quotes/:quoteId'><QuoteDetail /></Route>
+        <Route path='/new-quote'><NewQuote /></Route>
+      </Switch>
+    </Layout>
   );
 }
 
