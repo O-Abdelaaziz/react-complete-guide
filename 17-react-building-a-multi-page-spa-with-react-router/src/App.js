a8049dc1eaaea5392d7bf06464c3729e983e4d65
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-23 12:36:37 +0200

    Task 166: adding dynamic routes with params (like angular => /:vlue)

--- 17-react-building-a-multi-page-spa-with-react-router/src/App.js
+++ 17-react-building-a-multi-page-spa-with-react-router/src/App.js
@@ -1,6 +1,7 @@
 import { Route } from "react-router-dom";
 import MainHeader from "./components/MainHeader";
 import Product from "./pages/Product";
+import ProductDetails from "./pages/ProductDetails";
 import Welcome from "./pages/Welcome";
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <MainHeader/>
       <Route path='/welcome'><Welcome/></Route>
       <Route path='/product'><Product/></Route>
+      <Route path='/product-details/:productId'><ProductDetails/></Route>
     </div>
   );
 }
--- 17-react-building-a-multi-page-spa-with-react-router/src/pages/Product.js
+++ 17-react-building-a-multi-page-spa-with-react-router/src/pages/Product.js
@@ -4,6 +4,11 @@ const Product = () => {
     return (
         <div>
             <h1>Hello From Product Component.</h1>
+            <ul>
+            <li>Product Details 1</li>
+            <li>Product Details 2</li>
+            <li>Product Details 3</li>
+           </ul> 
         </div>
     )
 }
