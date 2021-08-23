e3fda35372a518429835f84e8f16bc3f12fc3cfb
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-23 21:21:54 +0200

    Task 176: outputting data on the details page

--- 18-react-complete-practice-project_4/src/components/pages/QuoteDetail.js
+++ 18-react-complete-practice-project_4/src/components/pages/QuoteDetail.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import {Route, useParams} from 'react-router-dom';
 import Comments from '../comments/Comments';
+import HighlightedQuote from '../quotes/HighlightedQuote';
+
+const DUMMY_QUOTES=[
+    {id:'q1',author:'Abdelaaziz',text:'Learning react from scratch'},
+    {id:'q2',author:'Abdelaaziz',text:'Learning react router from scratch'},
+]
+
 const QuoteDetail = () => {
     const params = useParams();
+    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+    
+    if(!quote){
+        return <p>no quote found!</p>
+    }
+    
     //const dynamicParam=`/quotes/${params.quoteId}/comments`;
     return (
         <div>
-            <h1>quote detail</h1>
-            <p>ID: {params.quoteId}</p>
+            <HighlightedQuote text={quote.text} author={quote.author}/> 
             <Route path={`/quotes/${params.quoteId}/comments`}><Comments/></Route>
         </div>
     )
