75f37332a36c2b5a575bf4990cf058daf3c3e312
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-23 19:14:23 +0200

    Task 173: practicing nested routes

--- 18-react-complete-practice-project_4/src/components/pages/QuoteDetail.js
+++ 18-react-complete-practice-project_4/src/components/pages/QuoteDetail.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import {useParams} from 'react-router-dom';
+import {Route, useParams} from 'react-router-dom';
+import Comments from '../comments/Comments';
 const QuoteDetail = () => {
     const params = useParams();
+    //const dynamicParam=`/quotes/${params.quoteId}/comments`;
     return (
         <div>
             <h1>quote detail</h1>
             <p>ID: {params.quoteId}</p>
+            <Route path={`/quotes/${params.quoteId}/comments`}><Comments/></Route>
         </div>
     )
 }
