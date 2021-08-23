6405eb0843461483965388afd474f71b39e439c9
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-23 13:14:40 +0200

    Task 169: working with nested routes (route must be active to be evaluated)

--- 17-react-building-a-multi-page-spa-with-react-router/src/pages/Welcome.js
+++ 17-react-building-a-multi-page-spa-with-react-router/src/pages/Welcome.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link, Route } from 'react-router-dom'
 
 const Welcome = () => {
     return (
         <div>
             <h1>Hello From Welcome Component.</h1>
+            <Link to='/welcome/new-user'>user profile</Link>
+            {/* if welcome page is active this route will be
+             evaluated , if not so never will be executed */}
+            <Route path='/welcome/new-user'>
+                <p>welcome new, user!</p>
+            </Route>
         </div>
     )
 }
