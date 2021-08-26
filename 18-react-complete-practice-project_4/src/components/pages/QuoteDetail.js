1e52a044169e5c6662c7898aadd60f77f8c69c97
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-26 19:07:39 +0200

    Task 180: getting creative with nested routes (we can also hide/show content using react route)

--- 18-react-complete-practice-project_4/src/components/pages/QuoteDetail.js
+++ 18-react-complete-practice-project_4/src/components/pages/QuoteDetail.js
@@ -1,26 +1,32 @@
 import React from 'react'
-import {Route, useParams} from 'react-router-dom';
+import { Link, Route, useParams } from 'react-router-dom';
 import Comments from '../comments/Comments';
 import HighlightedQuote from '../quotes/HighlightedQuote';
 
-const DUMMY_QUOTES=[
-    {id:'q1',author:'Abdelaaziz',text:'Learning react from scratch'},
-    {id:'q2',author:'Abdelaaziz',text:'Learning react router from scratch'},
+const DUMMY_QUOTES = [
+    { id: 'q1', author: 'Abdelaaziz', text: 'Learning react from scratch' },
+    { id: 'q2', author: 'Abdelaaziz', text: 'Learning react router from scratch' },
 ]
 
 const QuoteDetail = () => {
     const params = useParams();
     const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
-    
-    if(!quote){
+
+    if (!quote) {
         return <p>no quote found!</p>
     }
-    
+
     //const dynamicParam=`/quotes/${params.quoteId}/comments`;
     return (
         <div>
-            <HighlightedQuote text={quote.text} author={quote.author}/> 
-            <Route path={`/quotes/${params.quoteId}/comments`}><Comments/></Route>
+            <HighlightedQuote text={quote.text} author={quote.author} />
+            <Route path={`/quotes/${params.quoteId}`} exact>
+                <div className='centered'>
+                    <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>Load Comments</Link>
+                </div>
+            </Route>
+
+            <Route path={`/quotes/${params.quoteId}/comments`}><Comments /></Route>
         </div>
     )
 }
