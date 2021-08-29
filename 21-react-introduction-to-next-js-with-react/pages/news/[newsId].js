738234bd1e236b1ea4039f14757187ba09f1d53c
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-29 16:49:04 +0200

    Task 206: linking between pages (using link component from nextjs)

--- 21-react-introduction-to-next-js-with-react/pages/news/[newsId].js
+++ 21-react-introduction-to-next-js-with-react/pages/news/[newsId].js
@@ -1,10 +1,14 @@
 import { useRouter } from "next/router";
+import { Fragment } from "react";
 
 function DetailsPage() {
     const router = useRouter();
     const newsId = router.query.newsId
     console.log(newsId);
-    return <h1>Details Page</h1>
+    return <Fragment>
+        <h1>Details Page</h1>
+        <p>{`You are in ${newsId}`}</p>
+    </Fragment> 
 }
 
 export default DetailsPage;
--- 21-react-introduction-to-next-js-with-react/pages/news/index.js
+++ 21-react-introduction-to-next-js-with-react/pages/news/index.js
@@ -1,5 +1,16 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
 function NewsPage() {
-    return <h1>News Page</h1>
+    return (<Fragment>
+        <h1>The News Page</h1>
+        <ul>
+            <li>
+                <Link href='/news/next-js-is-great-framework'>NextJs is great framework</Link>
+            </li>
+            <li>Something else</li>
+        </ul>
+    </Fragment>);
 }
 
 export default NewsPage;
