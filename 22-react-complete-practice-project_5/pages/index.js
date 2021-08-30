a02520b3d6ced265e8bdc2296aa38cd85da96b3c
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-30 11:55:04 +0200

    Task 223: adding _head_ metadata

--- 22-react-complete-practice-project_5/pages/index.js
+++ 22-react-complete-practice-project_5/pages/index.js
@@ -1,12 +1,23 @@
 import { MongoClient } from "mongodb";
+import Head from "next/head";
+import { Fragment } from "react";
 import Layout from "../components/layout/Layout";
 import MeetupList from "../components/meetups/MeetupList";
 
 function HomePage(props) {
 
-    return (
-        <MeetupList meetups={props.meetups} />
-    );
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetup</title>
+        <meta
+          name='description'
+          content='Browse a huge list of highly active React meetups!'
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </Fragment>
+  );
 }
 
 // export async function getServerSideProps(context) {
@@ -20,24 +31,24 @@ function HomePage(props) {
 // };
 //like cache system (Laravel !!!)
 export async function getStaticProps() {
-    // fetch data from an API
+  // fetch data from an API
 
-    const client = await MongoClient.connect("mongodb://admin:admin@cluster0-shard-00-00.ngw9q.mongodb.net:27017,cluster0-shard-00-01.ngw9q.mongodb.net:27017,cluster0-shard-00-02.ngw9q.mongodb.net:27017/meetups?ssl=true&replicaSet=atlas-420w61-shard-0&authSource=admin&retryWrites=true&w=majority");
-    const db = client.db();
-    const meetupsCollection = db.collection('meetups');
-    const result = await meetupsCollection.find().toArray();
-    console.log(result);
-    client.close();
-    return {
-      props: {
-        meetups: result.map((meetup) => ({
-          title: meetup.title,
-          address: meetup.address,
-          image: meetup.image,
-          id: meetup._id.toString(),
-        })),
-      },
-      revalidate: 1
-    }; 
-  }
+  const client = await MongoClient.connect("mongodb://admin:admin@cluster0-shard-00-00.ngw9q.mongodb.net:27017,cluster0-shard-00-01.ngw9q.mongodb.net:27017,cluster0-shard-00-02.ngw9q.mongodb.net:27017/meetups?ssl=true&replicaSet=atlas-420w61-shard-0&authSource=admin&retryWrites=true&w=majority");
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+  const result = await meetupsCollection.find().toArray();
+  console.log(result);
+  client.close();
+  return {
+    props: {
+      meetups: result.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1
+  };
+}
 export default HomePage;
