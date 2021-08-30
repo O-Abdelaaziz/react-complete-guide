6eb933513486a217b520a4252d3df33dd32d3888
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-30 14:19:31 +0200

    Task 226: preparing the demo project

--- 23-react-animating-react-apps/src/App.js
+++ 23-react-animating-react-apps/src/App.js
@@ -6,13 +6,24 @@ import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
 class App extends Component {
+  state = {
+    modalIsOpen: false,
+  }
+
+  openModalHandler() {
+    this.setState({ modalIsOpen: true });
+  }
+  closeModalHandler() {
+    this.setState({ modalIsOpen: false });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal />
+        <Modal show={this.state.modalIsOpen} closed={this.closeModalHandler} />
         <Backdrop />
-        <button className="Button">Open Modal</button>
+        <button className="Button" onClick={this.openModalHandler}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
       </div>
