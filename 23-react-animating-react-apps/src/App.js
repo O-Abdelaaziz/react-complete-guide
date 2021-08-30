43c1802f402d6f819beb44702a19fa54230b6e91
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-30 16:00:19 +0200

    Task 230: using react transition-group

--- 23-react-animating-react-apps/src/App.js
+++ 23-react-animating-react-apps/src/App.js
@@ -4,11 +4,12 @@ import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
-
+import Transition from "react-transition-group/Transition";
 class App extends Component {
-
+  Trans
   state = {
     modalIsOpen: false,
+    showBlock: false
   }
 
   openModalHandler = () => {
@@ -22,6 +23,30 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button className="Button" onClick={() => this.setState(prevState => ({ showBlock: !prevState.showBlock }))}>Toggle</button>
+        <br /><br />
+        {/* {this.state.showBlock ? */}
+        <Transition 
+        in={this.state.showBlock} 
+        timeout={1000}
+        mountOnEnter
+        unmountOnExit
+        >
+          {state =>
+            <div style={{
+              backgroundColor: 'red',
+              width: 100,
+              height: 100,
+              margin: 'auto',
+              transition:'opacity 1s ease-out',
+              opacity: state === 'exiting' ? 0 : 1
+            }}></div>
+
+          }
+
+        </Transition>
+        {/* : null} */}
+        <br /><br />
         {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModalHandler} /> : null}
         {this.state.modalIsOpen ? <Backdrop /> : null}
         <button className="Button" onClick={this.openModalHandler}>Open Modal</button>
