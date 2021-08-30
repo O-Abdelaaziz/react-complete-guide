1c9d574e351a2094784406ae5d157aa1d34a47ef
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-30 11:21:49 +0200

    Task 222: getting meetup details data & preparing pages

--- 22-react-complete-practice-project_5/pages/[meetupId]/index.js
+++ 22-react-complete-practice-project_5/pages/[meetupId]/index.js
@@ -1,49 +1,62 @@
+import { MongoClient, ObjectId } from "mongodb";
 import { Fragment } from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetailsPage() {
+function MeetupDetailsPage(props) {
 
     return (
-        <Fragment>
-            <MeetupDetail
-                image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
-                title='First Meetup'
-                address='Some Street 5, Some City'
-                description='This is a first meetup'
-            />
-        </Fragment>
+        <MeetupDetail
+            image={props.meetupData.image}
+            title={props.meetupData.title}
+            address={props.meetupData.address}
+            description={props.meetupData.description}
+        />
     );
 }
 export async function getStaticPaths() {
-return{
-    fallback:false,
-    paths:[
-        {
-            params:{
-                meetupId:'m1'
+    const client = await MongoClient.connect("mongodb://admin:admin@cluster0-shard-00-00.ngw9q.mongodb.net:27017,cluster0-shard-00-01.ngw9q.mongodb.net:27017,cluster0-shard-00-02.ngw9q.mongodb.net:27017/meetups?ssl=true&replicaSet=atlas-420w61-shard-0&authSource=admin&retryWrites=true&w=majority");
+    const db = client.db();
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.find({}, { _id: 1 }).toArray();
+    console.log(result);
+    client.close();
+
+    return {
+        fallback: false,
+
+        paths: result.map(item => (
+
+            {
+                params: {
+                    meetupId: item._id.toString()
+                }
             }
-        },
-        {
-            params:{
-                meetupId:'m2'
-            }
-        },
-    ]
-} 
+
+        ))
+    }
 }
 
 export async function getStaticProps(context) {
+
     const meetupId = context.params.meetupId;
     //this can't log in browser console ... it's running during build time in ide console
     console.log(meetupId);
+
+    const client = await MongoClient.connect("mongodb://admin:admin@cluster0-shard-00-00.ngw9q.mongodb.net:27017,cluster0-shard-00-01.ngw9q.mongodb.net:27017,cluster0-shard-00-02.ngw9q.mongodb.net:27017/meetups?ssl=true&replicaSet=atlas-420w61-shard-0&authSource=admin&retryWrites=true&w=majority");
+    const db = client.db();
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.findOne({ _id: ObjectId(meetupId) });
+    console.log(result);
+    client.close();
+
     return {
         props: {
             meetupData: {
-                id:meetupId,
-                image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-                title:'First Meetup',
-                address:'Some Street 5, Some City',
-                description:'This is a first meetup',
+                id: result._id.toString(),
+                title: result.title,
+                image: result.image,
+                address: result.address,
+                description: result.description
             },
         }
     };
