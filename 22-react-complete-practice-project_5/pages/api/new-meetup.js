ee532e0b4d8fe7c914745d9ccb902e88bd69b0e2
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-30 08:30:49 +0200

    Task 219: working with mongodb

--- 22-react-complete-practice-project_5/pages/api/new-meetup.js
+++ 22-react-complete-practice-project_5/pages/api/new-meetup.js
@@ -1,7 +1,15 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body;
-        
-        const { title, image, address, description } = data
+
+        const client = await MongoClient.connect("mongodb+srv://admin:admin@cluster0.ngw9q.mongodb.net/meetups?retryWrites=true&w=majority");
+        const db = client.db();
+        const meetupsCollection = db.collection('meetups');
+        const result = await meetupsCollection.insertOne(data);
+        console.log(result);
+        client.close();
+        res.status(201).json({message:'meetup inserted.'});
     }
 }
