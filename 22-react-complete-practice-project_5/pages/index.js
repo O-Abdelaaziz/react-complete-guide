b26fb20fe6587e9c442fea5d1b8d3769e82e0e8d
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-30 10:15:05 +0200

    Task 221: getting data from the database

--- 22-react-complete-practice-project_5/pages/index.js
+++ 22-react-complete-practice-project_5/pages/index.js
@@ -1,22 +1,6 @@
+import { MongoClient } from "mongodb";
 import Layout from "../components/layout/Layout";
 import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'A First Meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Some address 5, 12345 Some City',
-        description: 'This is a first meetup!'
-    },
-    {
-        id: 'm2',
-        title: 'A Second Meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Some address 10, 12345 Some City',
-        description: 'This is a second meetup!'
-    }
-];
 
 function HomePage(props) {
 
@@ -37,9 +21,21 @@ function HomePage(props) {
 //like cache system (Laravel !!!)
 export async function getStaticProps() {
     // fetch data from an API
+
+    const client = await MongoClient.connect("mongodb://admin:admin@cluster0-shard-00-00.ngw9q.mongodb.net:27017,cluster0-shard-00-01.ngw9q.mongodb.net:27017,cluster0-shard-00-02.ngw9q.mongodb.net:27017/meetups?ssl=true&replicaSet=atlas-420w61-shard-0&authSource=admin&retryWrites=true&w=majority");
+    const db = client.db();
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.find().toArray();
+    console.log(result);
+    client.close();
     return {
       props: {
-        meetups: DUMMY_MEETUPS
+        meetups: result.map((meetup) => ({
+          title: meetup.title,
+          address: meetup.address,
+          image: meetup.image,
+          id: meetup._id.toString(),
+        })),
       },
       revalidate: 1
     }; 
