75ebd9d0cecccc00ce1a6e79a76033e827660620
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-08-31 19:23:28 +0200

    Task 242: finishing the store hook

--- 24-react-replacing-redux-with-react-hooks/src/hooks-store/hook-store.js
+++ 24-react-replacing-redux-with-react-hooks/src/hooks-store/hook-store.js
@@ -2,16 +2,31 @@ import { useEffect, useState } from "react";
 
 let globalState = {};
 let listeners = [];
-let action = {};
+let actions = {};
 
 const useStore = () => {
     const setState = useState(globalState)[1];
+    const dispatch = actionIdentifier => {
+        const newState = actions[actionIdentifier](globalState)
+        globalState = { ...globalState, ...newState };
+
+        for (const listener of listeners) {
+            listener(globalState)
+        }
+    }
     useEffect(() => {
         listeners.push(setState);
         return () => {
             listeners = listeners.filter((li) => li !== setState);
         };
     }, [setState]);
+
+    return [globalState, dispatch];
 };
 
-export default useStore;
+export const initState = (userActions, initState) => {
+    if (initState) {
+        globalState = { ...globalState, ...initState };
+        actions = { ...actions, ...userActions };
+    }
+};
