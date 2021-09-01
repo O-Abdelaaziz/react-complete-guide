792b44ac18de266c1ec9372d9f81edc9e29e73a0
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-01 19:01:39 +0200

    Task 251: working with refs & useRef

--- 25-react-working-with-react-type-script/src/components/NewTodo.tsx
+++ 25-react-working-with-react-type-script/src/components/NewTodo.tsx
@@ -1,14 +1,19 @@
 import React, { useRef } from "react";
 
-const NewTodo = () => {
+const NewTodo: React.FC = (props) => {
+  const inputTextRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+    const enteredText = inputTextRef.current?.value;
+    if(enteredText?.trim().length === 0){
+        return;
+    }
   };
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="todo-text">Todo Text</label>
-      <input type="text" id="todo-text"/>
+      <input type="text" id="todo-text" ref={inputTextRef} />
       <button>Add Todo</button>
     </form>
   );
