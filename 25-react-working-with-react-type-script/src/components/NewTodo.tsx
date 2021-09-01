c6316b67602d3cab121370babc16f97a9b5c6d2b
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-01 18:28:35 +0200

    Task 250: form submissions in typescript projects

---
+++ 25-react-working-with-react-type-script/src/components/NewTodo.tsx
@@ -0,0 +1,17 @@
+import React, { useRef } from "react";
+
+const NewTodo = () => {
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="todo-text">Todo Text</label>
+      <input type="text" id="todo-text"/>
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
