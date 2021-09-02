3df263449ddb9c7fb66508c948fd19a5e00b5aac
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-02 19:43:35 +0200

    Task 261: multiple states

--- 26-react-optional-react-hook-recap-summary/src/components/Ingredients/IngredientForm.js
+++ 26-react-optional-react-hook-recap-summary/src/components/Ingredients/IngredientForm.js
@@ -4,26 +4,15 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  // const [titleInput, setTitleInput] = useState('');
-  // const [amountInput, setAmount] = useState('');
-  const [inputState,setInputState] = useState({ title: '', amount: '' });
+  const [titleInput, setTitleInput] = useState('');
+  const [amountInput, setAmount] = useState('');
 
   const titleInputChangeHandler = (event) => {
-    //setTitleInput(event.target.value);
-    const newTitle=event.target.value;
-    setInputState((prevState) => {
-      return { title: newTitle, amount: prevState.amount }
-    });
-    console.log(inputState.title);
+    setTitleInput(event.target.value);
   }
 
   const amountInputChangeHandler = (event) => {
-    //setAmount(event.target.value);
-    const newAmount=event.target.value;
-    setInputState((prevState) => {
-      return { amount: newAmount, title: prevState.title }
-    });
-    console.log(inputState.amount);
+    setAmount(event.target.value);
   }
 
   const submitHandler = event => {
@@ -37,11 +26,11 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState.title} onChange={titleInputChangeHandler} />
+            <input type="text" id="title" value={titleInput} onChange={titleInputChangeHandler} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState.amount} onChange={amountInputChangeHandler} />
+            <input type="number" id="amount" value={amountInput} onChange={amountInputChangeHandler} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
