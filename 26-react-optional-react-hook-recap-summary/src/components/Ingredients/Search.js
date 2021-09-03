024730a69dc47c26efe238a02fceccf5c5c8cbb5
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-03 14:33:49 +0200

    Task 267: working with ref use-ref

--- 26-react-optional-react-hook-recap-summary/src/components/Ingredients/Search.js
+++ 26-react-optional-react-hook-recap-summary/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,35 +6,40 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onLoadedIngredients } = props;
   const [searchInput, setSearchInput] = useState('');
+  const inputRef = useRef();
 
   const onChangeSearchInput = (event) => {
     setSearchInput(event.target.value);
   }
 
   useEffect(() => {
-    const query = searchInput.length === 0 ? '' : `?orderBy="title"&equalTo="${searchInput}"`;
-    fetch('https://react-http-c7642-default-rtdb.firebaseio.com/ingredients.json' +query)
-      .then(
-        response => response.json()
-      ).then(responseData => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
+    setTimeout(() => {
+      if (searchInput === inputRef.current.value) {
+        const query = searchInput.length === 0 ? '' : `?orderBy="title"&equalTo="${searchInput}"`;
+        fetch('https://react-http-c7642-default-rtdb.firebaseio.com/ingredients.json' + query)
+          .then(
+            response => response.json()
+          ).then(responseData => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            onLoadedIngredients(loadedIngredients);
           });
-        }
-        onLoadedIngredients(loadedIngredients);
-      });
-  }, [searchInput, onLoadedIngredients]);
+      }
+    }, 500);
+  }, [searchInput, onLoadedIngredients,inputRef]);
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={searchInput} onChange={onChangeSearchInput} />
+          <input type="text" ref={inputRef} value={searchInput} onChange={onChangeSearchInput} />
         </div>
       </Card>
     </section>
