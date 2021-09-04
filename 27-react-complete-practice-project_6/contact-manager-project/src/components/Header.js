94e1c41a714a1fe38ae2265cb6dc3f94b65579b5
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-04 19:43:56 +0200

    Task 280: working with header component

--- 27-react-complete-practice-project_6/contact-manager-project/src/components/Header.js
+++ 27-react-complete-practice-project_6/contact-manager-project/src/components/Header.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <h2>Contact Manager</h2>
+            </div>
         </div>
     )
 }
