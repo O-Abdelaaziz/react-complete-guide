55915d9868f507e0f691f9723361d9c20595ea71
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-05 15:59:15 +0200

    Task 288: working with react router (navigation setup)

--- 27-react-complete-practice-project_6/contact-manager-project/src/components/AddContact.js
+++ 27-react-complete-practice-project_6/contact-manager-project/src/components/AddContact.js
@@ -1,22 +1,25 @@
 import React, { useRef } from 'react'
+import { useHistory } from 'react-router';
 
 const AddContact = (props) => {
     const nameInputRef = useRef();
     const emailInputRef = useRef();
+    const history = useHistory();
 
-    const submitHandler=(event)=>{
+    const submitHandler = (event) => {
         event.preventDefault();
-        const name=nameInputRef.current.value;
-        const email=emailInputRef.current.value;
+        const name = nameInputRef.current.value;
+        const email = emailInputRef.current.value;
 
-        if(name ==='' || email=== ''){
+        if (name === '' || email === '') {
             alert("name or email can't be empty!");
             return;
         }
-        props.onAddContact({name,email});
+        props.onAddContact({ name, email });
 
-        nameInputRef.current.value='';
-        emailInputRef.current.value='';
+        nameInputRef.current.value = '';
+        emailInputRef.current.value = '';
+        history.push('/contacts')
     }
     return (
         <div className="ui main">
@@ -24,11 +27,11 @@ const AddContact = (props) => {
             <form className="ui form" onSubmit={submitHandler}>
                 <div className="field">
                     <label htmlFor="name">Name</label>
-                    <input id="name" type="text" name="name" placeholder="contact name" ref={nameInputRef}/>
+                    <input id="name" type="text" name="name" placeholder="contact name" ref={nameInputRef} />
                 </div>
                 <div className="field">
                     <label htmlFor="email">Email</label>
-                    <input id="email" type="email" name="email" placeholder="contact email" ref={emailInputRef}/>
+                    <input id="email" type="email" name="email" placeholder="contact email" ref={emailInputRef} />
                 </div>
                 <button className="ui button blue">Save</button>
             </form>
