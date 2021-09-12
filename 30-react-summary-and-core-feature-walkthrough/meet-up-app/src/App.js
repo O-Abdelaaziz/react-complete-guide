861e422b389ffd597d10fc8f61555db0cae6419a
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-12 21:05:24 +0200

    Task 328: adding link & navigation

--- 30-react-summary-and-core-feature-walkthrough/meet-up-app/src/App.js
+++ 30-react-summary-and-core-feature-walkthrough/meet-up-app/src/App.js
@@ -1,25 +1,29 @@
 import { Switch, Route, Redirect } from "react-router-dom";
+import MainNavigation from "./components/layout/MainNavigation";
 import AllMeetup from "./pages/AllMeetup";
 import Favorites from "./pages/Favorites";
 import NewMeetup from "./pages/NewMeetup";
 
 function App() {
-  return <div>
-    <Switch>
-      <Route path='/' exact>
-        <Redirect to='/meetups' />
-      </Route>
-      <Route path='/meetups'>
-        <AllMeetup />
-      </Route>
-      <Route path='/new-meetup'>
-        <NewMeetup />
-      </Route>
-      <Route path='/favorites'>
-        <Favorites />
-      </Route>
-    </Switch>
-  </div>;
+  return (
+    <div>
+      <MainNavigation/>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/meetups' />
+        </Route>
+        <Route path='/meetups'>
+          <AllMeetup />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetup />
+        </Route>
+        <Route path='/favorites'>
+          <Favorites />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ 30-react-summary-and-core-feature-walkthrough/meet-up-app/src/components/layout/MainNavigation.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const MainNavigation = () => {
+    return (
+        <header>
+            <div>React Meetups</div>
+            <nav>
+                <ul>
+                    <li><Link to='/'>Meetups</Link></li>
+                    <li><Link to='/new-meetup'>New Meetup</Link></li>
+                    <li><Link to='/favorites'>Favorites</Link></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default MainNavigation
