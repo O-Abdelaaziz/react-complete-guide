55dd5721f06e47bf0de2ebb6be79d61e41670fde
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-12 20:57:29 +0200

    Task 327: adding routing (react-router-dom)

--- 30-react-summary-and-core-feature-walkthrough/meet-up-app/src/App.js
+++ 30-react-summary-and-core-feature-walkthrough/meet-up-app/src/App.js
@@ -1,5 +1,25 @@
+import { Switch, Route, Redirect } from "react-router-dom";
+import AllMeetup from "./pages/AllMeetup";
+import Favorites from "./pages/Favorites";
+import NewMeetup from "./pages/NewMeetup";
+
 function App() {
-  return <div></div>;
+  return <div>
+    <Switch>
+      <Route path='/' exact>
+        <Redirect to='/meetups' />
+      </Route>
+      <Route path='/meetups'>
+        <AllMeetup />
+      </Route>
+      <Route path='/new-meetup'>
+        <NewMeetup />
+      </Route>
+      <Route path='/favorites'>
+        <Favorites />
+      </Route>
+    </Switch>
+  </div>;
 }
 
 export default App;
---
+++ 30-react-summary-and-core-feature-walkthrough/meet-up-app/src/pages/AllMeetup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllMeetup = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AllMeetup
