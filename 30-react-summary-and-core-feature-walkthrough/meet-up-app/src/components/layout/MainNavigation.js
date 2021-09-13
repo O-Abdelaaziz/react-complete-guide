2711a1dc3b13f5143104640316a0f1d0908a17bb
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-13 12:46:17 +0200

    Task 341: more context usage & final step

--- 30-react-summary-and-core-feature-walkthrough/meet-up-app/src/components/layout/MainNavigation.js
+++ 30-react-summary-and-core-feature-walkthrough/meet-up-app/src/components/layout/MainNavigation.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import FavoritesContext from '../../store/favorites-context';
 import classes from "./MainNavigation.module.css";
 const MainNavigation = () => {
+    const context = useContext(FavoritesContext);
+    const totalFavorites = context.totalFavorites;
+
     return (
         <header className={classes.header}>
             <div className={classes.logo}>React Meetups</div>
@@ -9,7 +13,7 @@ const MainNavigation = () => {
                 <ul>
                     <li><Link to='/'>Meetups</Link></li>
                     <li><Link to='/new-meetup'>New Meetup</Link></li>
-                    <li><Link to='/favorites'>Favorites</Link></li>
+                    <li><Link to='/favorites'>Favorites <span className={classes.badge}> {totalFavorites}</span></Link></li>
                 </ul>
             </nav>
         </header>
--- 30-react-summary-and-core-feature-walkthrough/meet-up-app/src/pages/Favorites.js
+++ 30-react-summary-and-core-feature-walkthrough/meet-up-app/src/pages/Favorites.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import MeetupList from '../components/meetups/MeetupList';
+import FavoritesContext from '../store/favorites-context';
 
 const Favorites = () => {
+    const context = useContext(FavoritesContext);
+    const meetups=context.favorites;
+
+    let content;
+    if (context.totalFavorites === 0) {
+        content = <p>You got no favorites yet. Start adding some?</p>;
+      } else {
+        content = <MeetupList meetups={meetups} />;
+      }
+
     return (
-        <div>
-            
-        </div>
+        <section>
+            <h1>Meetups</h1>
+            {content}
+        </section>
     )
 }
 
