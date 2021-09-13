4f669d2fbb7754eac7f8cf00d6ca0b9aa693b88e
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-13 09:50:35 +0200

    Task 335: preparing the app for http

--- 30-react-summary-and-core-feature-walkthrough/meet-up-app/src/components/meetups/NewMeetupForm.js
+++ 30-react-summary-and-core-feature-walkthrough/meet-up-app/src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css';
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -10,7 +10,7 @@ function NewMeetupForm() {
 
 
   function submitHandler(event) {
-    
+
     event.preventDefault();
     const enteredTitle = titleInputRef.current.value;
     const enteredImage = imageInputRef.current.value;
@@ -23,6 +23,8 @@ function NewMeetupForm() {
       address: enteredAddress,
       description: enteredDescription,
     };
+
+    props.onAddMeetup(meetupData);
   }
 
   return (
--- 30-react-summary-and-core-feature-walkthrough/meet-up-app/src/pages/NewMeetup.js
+++ 30-react-summary-and-core-feature-walkthrough/meet-up-app/src/pages/NewMeetup.js
@@ -2,10 +2,15 @@ import React from 'react'
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 const NewMeetup = () => {
+
+    const addMeetupHandler= ()=>{
+
+    }
+
     return (
         <div>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </div>
     )
 }
